04ca224d7487f8440305b745813b6799c439c57d
sudhish-rithvik <150578774+sudhish-rithvik@users.noreply.github.com>
2024-09-20 06:12:36 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ app/db.ts
@@ -0,0 +1,50 @@
+import { drizzle } from 'drizzle-orm/postgres-js';
+import { pgTable, serial, varchar } from 'drizzle-orm/pg-core';
+import { eq } from 'drizzle-orm';
+import postgres from 'postgres';
+import { genSaltSync, hashSync } from 'bcrypt-ts';
+
+// Optionally, if not using email/pass login, you can
+// use the Drizzle adapter for Auth.js / NextAuth
+// https://authjs.dev/reference/adapter/drizzle
+let client = postgres(`${process.env.POSTGRES_URL!}?sslmode=require`);
+let db = drizzle(client);
+
+export async function getUser(email: string) {
+  const users = await ensureTableExists();
+  return await db.select().from(users).where(eq(users.email, email));
+}
+
+export async function createUser(email: string, password: string) {
+  const users = await ensureTableExists();
+  let salt = genSaltSync(10);
+  let hash = hashSync(password, salt);
+
+  return await db.insert(users).values({ email, password: hash });
+}
+
+async function ensureTableExists() {
+  const result = await client`
+    SELECT EXISTS (
+      SELECT FROM information_schema.tables 
+      WHERE table_schema = 'public' 
+      AND table_name = 'User'
+    );`;
+
+  if (!result[0].exists) {
+    await client`
+      CREATE TABLE "User" (
+        id SERIAL PRIMARY KEY,
+        email VARCHAR(64),
+        password VARCHAR(64)
+      );`;
+  }
+
+  const table = pgTable('User', {
+    id: serial('id').primaryKey(),
+    email: varchar('email', { length: 64 }),
+    password: varchar('password', { length: 64 }),
+  });
+
+  return table;
+}
